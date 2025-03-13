ec08a1048e5cd48c3f3e0acd30d0abe79bbf0b06
kodali-98 <bhavanakodali98@gmail.com>
2025-03-13 15:38:31 -0400

    Added and modified

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,22 +1,54 @@
+/*pipeline {
+    agent any
+
+     stages {
+        stage('Clone Repository') {
+            steps {
+                git 'https://github.com/your-user/RockPaperScissorgame.git'
+            }
+        }
+
+        stage('Deploy to Server') {
+            steps {
+                script {
+                    sh '''
+                    # Define your deployment steps
+                    echo "Deploying to server..."
+                    cp -r * /var/www/html/  # Copy files to Apache/Nginx root
+                    '''
+            }
+        }
+           }   }
+} */
+// integate docker into ci/cd pipeline using jenkins
 pipeline {
     agent any
 
     stages {
-        stage('Build') {
+        stage('Clone Repository') {
             steps {
-                echo 'Building the project...'
-            }
-        }
-        
-        stage('Test') {
-            steps {
-                echo 'Running tests...'
+                git 'https://github.com/bhavana-1997/rockpaperscissors.git'
             }
         }
 
-        stage('Deploy') {
+        stage('Build Docker Image') {
             steps {
-                echo 'Deploying the application...'
+                sh 'docker build -t chowdary175/rockpaperscissors .'
+            }
+        }
+
+        stage('Push to Docker Hub') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                    sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
+                    sh 'docker push chowdary175/rockpaperscissors'
+                }
+            }
+        }
+
+        stage('Deploy Application') {
+            steps {
+                sh 'docker run -d -p 3000:3000 chowdary175/rockpaperscissors'
             }
         }
     }
